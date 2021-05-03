4733ae31a75c4d9ef95d8deadd3876ffd1df23d0
ShacharMarkovich <234shachar@gmail.com>
2021-05-03 22:27:08 +0300

    add comments to ints.csv
    start documenting the project

--- ints.py
+++ ints.py
@@ -1,35 +1,44 @@
-import argparse
 import os
 
 import pandas as pd
 from pandas.core.groupby.generic import SeriesGroupBy
 from sklearn.ensemble import RandomForestClassifier
+
 from lib import get_data, calc_measures, k_best, intersection
 
 k = 20
 tested: SeriesGroupBy = None
 
 
-def a(file):
-    df = pd.read_csv(f'calculated/{file}')
-    X, y = get_data(f'numeric_db/{file}')
+def calc_intersections_measures(file: str) -> pd.DataFrame:
+    """
+    For a given file, return a DataFrame which contains the
+    MinMax slop_rank with the other ranks (who not kind of slope_rank) features intersections' measures calculation,
+    and for the other ranks, calc the measures and add it to the DataFrame too.
 
-    ranks = list(df)[1:]
+    :param file: the input file name
+    :return:  measures' DataFrame
+    """
+    df = pd.read_csv(f'calculated/{file}')
+    x, y = get_data(f'numeric_db/{file}')
+
+    ranks = list(df)[1:]  # drop attributes name's column
     slope = 'slope_rank_minmax'
-    others = [r for r in ranks if not r.startswith('slope_rank')]
+    others = [rank for rank in ranks if not rank.startswith('slope_rank')]
 
     ranks = others + [slope]
-    bests = {rank: k_best(df, rank, k) for rank in ranks}
+    bests = {rank: k_best(df, rank, k) for rank in ranks}  # calc bests for each rank
 
-    group = tested.get_group(file)
-    cur = group.loc[group['rank'].isin(ranks)]
+    group = tested.get_group(file)  # get fit group of file's measures
+    cur = group.loc[group['rank'].isin(ranks)]  # select ranks which appear in `ranks`
     ret = pd.DataFrame(cur)
     ret.insert(2, '#features', k)
 
+    # calc the measures for each other rank, and add it to DataFrame:
     for other in others:
         b = intersection(bests[slope], bests[other])
         clf = RandomForestClassifier(n_estimators=100)
-        mes = calc_measures(clf, X[b], y)
+        mes = calc_measures(clf, x[b], y)
 
         mes['file'] = file
         mes['rank'] = f'{slope} inter. {other}'
@@ -47,8 +56,9 @@ def main():
 
     df_sum = []
     for file in files:
-        df_sum.append(a(file))
+        df_sum.append(calc_intersections_measures(file))
 
+    # Concatenate the data and write it to csv file
     final = pd.concat(df_sum)
     final.set_index(['file', 'rank'], inplace=True)
     final.to_csv(f'ints.csv')
